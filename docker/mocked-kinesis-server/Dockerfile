43104788e6b50ff8f12d074d3657b1fe1de89bee
Thierry Nowak <thinow@gmail.com>
2020-11-20 17:07:44 +0100

    use mocked kinesis stream server

---
+++ docker/mocked-kinesis-server/Dockerfile
@@ -0,0 +1,6 @@
+FROM node:15.2.1-alpine3.12
+
+ENTRYPOINT ["kinesalite"]
+
+# see https://github.com/mhart/kinesalite
+RUN npm install -g kinesalite@3.3.3
--- tbd/stream_service.py
+++ tbd/stream_service.py
@@ -6,7 +6,7 @@ class StreamService:
     kinesis: KinesisClient
 
     def __init__(self) -> None:
-        self.kinesis = boto3.client('kinesis')
+        self.kinesis = boto3.client('kinesis', endpoint_url="http://localhost:4567")
 
     def get_names_of_streams(self) -> list:
         response = self.kinesis.list_streams()
