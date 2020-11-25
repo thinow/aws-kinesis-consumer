70520ab3eef2416c97a6e401bb66ea7fdbcbf3fb
Thierry Nowak <thinow@gmail.com>
2020-11-25 20:58:26 +0100

    MockedKinesis as a fixture

---
+++ tests/consumer/MockedKinesis.py
@@ -0,0 +1,34 @@
+import uuid
+
+import boto3
+from boto3_type_annotations.kinesis import Client as Kinesis
+
+
+class MockedKinesis:
+    endpoint: str
+    kinesis: Kinesis
+
+    def __init__(self, endpoint: str) -> None:
+        self.endpoint = endpoint
+        self.kinesis = boto3.client('kinesis', endpoint_url=endpoint)
+
+    def create_stream(self):
+        stream_name = f'mocked-stream-{uuid.uuid4()}'
+        self.kinesis.create_stream(
+            StreamName=stream_name,
+            ShardCount=1
+        )
+        return MockedStream(self.kinesis, stream_name)
+
+
+class MockedStream:
+    def __init__(self, kinesis: Kinesis, name: str) -> None:
+        self.kinesis = kinesis
+        self.name = name
+
+    def put_record(self, data: str):
+        self.kinesis.put_record(
+            StreamName=self.name,
+            PartitionKey=str(uuid.uuid4()),
+            Data=data.encode(),
+        )
--- tests/consumer/test_consumer.py
+++ tests/consumer/test_consumer.py
@@ -1,46 +1,13 @@
 import os
-import uuid
 from contextlib import redirect_stdout
 from io import StringIO
 from unittest import mock
 
-import boto3
-from boto3_type_annotations.kinesis import Client as Kinesis
+import pytest
 
 from aws_kinesis_consumer.configuration.configuration import Configuration
 from aws_kinesis_consumer.consumer.consumer import Consumer
-
-
-# TODO extract
-class MockedKinesis:
-    endpoint: str
-    kinesis: Kinesis
-
-    def __init__(self, endpoint: str) -> None:
-        self.endpoint = endpoint
-        self.kinesis = boto3.client('kinesis', endpoint_url=endpoint)
-
-    def create_stream(self):
-        stream_name = f'mocked-stream-{uuid.uuid4()}'
-        self.kinesis.create_stream(
-            StreamName=stream_name,
-            ShardCount=1
-        )
-        return MockedStream(self.kinesis, stream_name)
-
-
-class MockedStream:
-    def __init__(self, kinesis: Kinesis, name: str) -> None:
-        self.kinesis = kinesis
-        self.name = name
-
-    def put_record(self, data: str):
-        self.kinesis.put_record(
-            StreamName=self.name,
-            PartitionKey=str(uuid.uuid4()),
-            Data=data.encode(),
-        )
-
+from tests.consumer.MockedKinesis import MockedKinesis
 
 MOCKED_AWS_ENV_VARS = {
     "AWS_DEFAULT_REGION": "anything",
@@ -49,9 +16,13 @@ MOCKED_AWS_ENV_VARS = {
 }
 
 
-@mock.patch.dict(os.environ, MOCKED_AWS_ENV_VARS)
-def test_consume():
-    mocked_kinesis = MockedKinesis('http://localhost:4567/')
+@pytest.fixture()
+def mocked_kinesis() -> MockedKinesis:
+    with mock.patch.dict(os.environ, MOCKED_AWS_ENV_VARS):
+        yield MockedKinesis('http://localhost:4567/')
+
+
+def test_consume(mocked_kinesis: MockedKinesis):
     stream = mocked_kinesis.create_stream()
     stream.put_record('foo')
     stream.put_record('bar')
