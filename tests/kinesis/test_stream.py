e5cb30a55d46d2a77fec9bdf95494ac897ba9902
Thierry Nowak <thinow@gmail.com>
2020-11-30 21:49:49 +0100

    test stream preparation

--- tests/kinesis/test_stream.py
+++ tests/kinesis/test_stream.py
@@ -1,8 +1,10 @@
 import os
 from unittest import mock
+from unittest.mock import Mock, call
 
 import pytest
 from _pytest.capture import CaptureFixture
+from boto3_type_annotations.kinesis import Client
 
 from aws_kinesis_consumer.aws.aws_services_factory import AWSServicesFactory
 from aws_kinesis_consumer.configuration.configuration import Configuration, IteratorType
@@ -50,3 +52,108 @@ def test_consume(dockerized_kinesis: DockerizedKinesis, capsys: CaptureFixture):
 
 def split_unique_lines(getvalue: str) -> set:
     return set(getvalue.splitlines())
+
+
+def test_prepare_should_list_shards():
+    # given
+    stream, kinesis = create_stream_with_mocks(Configuration(
+        stream_name='any-stream-name',
+        iterator_type=IteratorType.LATEST,
+    ))
+
+    kinesis.list_shards.return_value = {
+        'Shards': [
+            {'ShardId': 'shard-id-001'},
+            {'ShardId': 'shard-id-002'},
+        ]
+    }
+
+    # when
+    stream.prepare()
+
+    # then
+    assert list(map(lambda s: s.shard_id, stream.shards)) == [
+        'shard-id-001',
+        'shard-id-002',
+    ]
+
+
+def test_prepare_should_filter_shards_based_on_iterator():
+    # given
+    stream, kinesis = create_stream_with_mocks(Configuration(
+        stream_name='any-stream-name',
+        iterator_type=IteratorType.TRIM_HORIZON,
+    ))
+
+    # when
+    stream.prepare()
+
+    # then
+    kinesis.list_shards.assert_called_with(
+        StreamName='any-stream-name',
+        ShardFilter={
+            'Type': 'AT_TRIM_HORIZON'
+        }
+    )
+
+
+def test_prepare_should_list_subsequent_shards():
+    # given
+    stream, kinesis = create_stream_with_mocks(Configuration(
+        stream_name='any-stream-name',
+        iterator_type=IteratorType.LATEST,
+    ))
+
+    kinesis.list_shards.side_effect = [
+        {'Shards': [{'ShardId': 'shard-id-001'}], 'NextToken': 'any', },
+        {'Shards': [{'ShardId': 'shard-id-002'}], 'NextToken': 'any', },
+        {'Shards': [{'ShardId': 'shard-id-003'}], },
+    ]
+
+    # when
+    stream.prepare()
+
+    # then
+    assert list(map(lambda s: s.shard_id, stream.shards)) == [
+        'shard-id-001',
+        'shard-id-002',
+        'shard-id-003',
+    ]
+
+
+def test_prepare_should_list_using_tokens():
+    # given
+    stream, kinesis = create_stream_with_mocks(Configuration(
+        stream_name='any-stream-name',
+        iterator_type=IteratorType.LATEST,
+    ))
+
+    kinesis.list_shards.side_effect = [
+        {'Shards': [], 'NextToken': 'first-test-token', },
+        {'Shards': [], 'NextToken': 'second-test-token', },
+        {'Shards': [], },
+    ]
+
+    # when
+    stream.prepare()
+
+    # then
+    assert kinesis.list_shards.call_args_list == [
+        call(StreamName='any-stream-name', ShardFilter={'Type': 'AT_LATEST'}),
+        call(NextToken='first-test-token'),
+        call(NextToken='second-test-token'),
+    ]
+
+
+def create_stream_with_mocks(configuration: Configuration):
+    kinesis: Client = Mock()
+    kinesis.list_shards.return_value = {
+        'Shards': []
+    }
+
+    aws_services_factory: AWSServicesFactory = Mock()
+    aws_services_factory.create_kinesis.return_value = kinesis
+
+    stream = Stream(aws_services_factory, configuration)
+
+    return stream, kinesis
