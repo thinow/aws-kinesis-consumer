6f38ad28218594cdfe8bdbd553640b82af6e9c9d
Thierry Nowak <thinow@gmail.com>
2020-11-30 21:49:48 +0100

    hide error when user interrupt loop

--- aws_kinesis_consumer/__main__.py
+++ aws_kinesis_consumer/__main__.py
@@ -5,16 +5,20 @@ from aws_kinesis_consumer.kinesis.stream import Stream
 
 
 def main(arguments: list):
-    ctx = Context()
+    try:
+        ctx = Context()
 
-    stream = Stream(
-        ctx.aws_services_factory,
-        ctx.configuration_factory.create_configuration(arguments)
-    )
+        stream = Stream(
+            ctx.aws_services_factory,
+            ctx.configuration_factory.create_configuration(arguments)
+        )
 
-    stream.prepare()
-    while True:
-        stream.print_records()
+        stream.prepare()
+        while True:
+            stream.print_records()
+
+    except KeyboardInterrupt:
+        pass
 
 
 if __name__ == '__main__':
