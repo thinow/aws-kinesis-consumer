bacd5ce6698d320839f37108d92a82be3dd01485
Thierry Nowak <thinow@gmail.com>
2020-11-30 21:49:48 +0100

    list all shards

--- aws_kinesis_consumer/kinesis/stream.py
+++ aws_kinesis_consumer/kinesis/stream.py
@@ -19,24 +19,46 @@ class Stream:
         self.shards = shards
 
     def find_shards(self, kinesis) -> tuple:
-        iterator_type: IteratorTypeProperties = self.configuration.iterator_type.value
-        response = kinesis.list_shards(
-            StreamName=self.configuration.stream_name,
-            ShardFilter={
-                'Type': iterator_type.shard_filter_type
-            }
-        )
+        shards_ids = self.find_shards_ids(kinesis)
 
         shards = map(
-            lambda shard_from_response: Shard(
-                shard_id=shard_from_response['ShardId'],
+            lambda id: Shard(
+                shard_id=id,
                 configuration=self.configuration,
                 kinesis=kinesis
             ),
-            response['Shards']
+            shards_ids
         )
 
         return tuple(shards)
+
+    def find_shards_ids(self, kinesis, next_token=None) -> list:
+        iterator_type: IteratorTypeProperties = self.configuration.iterator_type.value
+        if next_token is None:
+            response = kinesis.list_shards(
+                StreamName=self.configuration.stream_name,
+                ShardFilter={
+                    'Type': iterator_type.shard_filter_type
+                },
+            )
+        else:
+            response = kinesis.list_shards(
+                StreamName=self.configuration.stream_name,
+                ShardFilter={
+                    'Type': iterator_type.shard_filter_type
+                },
+                NextToken=next_token,
+            )
+
+        shards_ids = map(
+            lambda shard_response: shard_response['ShardId'],
+            response['Shards']
+        )
+
+        if 'NextToken' in response:
+            return list(shards_ids) + self.find_shards_ids(kinesis, response['NextToken'])
+        else:
+            return list(shards_ids)
 
     def print_records(self):
         [shard.print_records() for shard in self.shards]
