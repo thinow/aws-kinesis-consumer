b1d3fae92679a35ac6b3eff0e194723653b35ca0
Thierry Nowak <thinow@gmail.com>
2021-01-14 22:06:34 +0100

    error: handle unexpected errors

--- aws_kinesis_consumer/error/handler.py
+++ aws_kinesis_consumer/error/handler.py
@@ -1,9 +1,13 @@
+import sys
+
+
 class ErrorHandler:
 
     @classmethod
     def handle(cls, error: BaseException) -> None:
         # User intentionally interrupts the program. Ignore the exception and exit.
-        if isinstance(error, KeyboardInterrupt):
-            pass
-        else:
-            raise error
+        # if isinstance(error, KeyboardInterrupt):
+        #     pass
+        print(f'ERROR: the program stopped due to the following issue.', file=sys.stderr)
+        print(error, file=sys.stderr)
+        sys.exit(1)
--- tests/error/test_handler.py
+++ tests/error/test_handler.py
@@ -1,3 +1,26 @@
+import pytest
+
+from aws_kinesis_consumer import ErrorHandler
+
+handler = ErrorHandler()
+
+
+def test_unexpected_error(capsys):
+    # when
+    with pytest.raises(SystemExit) as raised_error:
+        handler.handle(RuntimeError('Unexpected error'))
+
+    # then
+    output = capsys.readouterr()
+    assert output.out.splitlines() == []
+    assert output.err.splitlines() == [
+        'ERROR: the program stopped due to the following issue.',
+        'Unexpected error',
+    ]
+
+    assert isinstance(raised_error.value, SystemExit)
+    assert raised_error.value.code == 1
+
 # TODO test key interrupt
 # TODO test NoRegionError
 # TODO test AWS credentials error (not found, session expired, invalid key id, secret, session token)
