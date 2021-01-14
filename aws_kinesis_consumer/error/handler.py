d1c5a0811420913371e24d2fb691ec3406c0762c
Thierry Nowak <thinow@gmail.com>
2021-01-14 22:06:55 +0100

    error: handle missing AWS region

--- aws_kinesis_consumer/error/handler.py
+++ aws_kinesis_consumer/error/handler.py
@@ -1,5 +1,7 @@
 import sys
 
+from botocore.exceptions import NoRegionError
+
 
 class ErrorHandler:
 
@@ -8,7 +10,12 @@ class ErrorHandler:
         # User intentionally interrupts the program. Ignore the exception and exit.
         if isinstance(error, KeyboardInterrupt):
             pass
+        elif isinstance(error, NoRegionError):
+            print('ERROR: AWS region has not been found.', file=sys.stderr)
+            print('Please pass the region using the environment variable AWS_DEFAULT_REGION. Example:', file=sys.stderr)
+            print('$ AWS_DEFAULT_REGION=eu-central-1 aws-kinesis-consumer --stream-name MyStream', file=sys.stderr)
+            sys.exit(1)
         else:
             print(f'ERROR: the program stopped due to the following issue.', file=sys.stderr)
-            print(error, file=sys.stderr)
+            print(repr(error), file=sys.stderr)
             sys.exit(1)
--- tests/error/test_handler.py
+++ tests/error/test_handler.py
@@ -1,10 +1,22 @@
+from botocore.exceptions import NoRegionError
 from pytest import CaptureFixture, raises
 
-# TODO test NoRegionError
+# TODO test bypass system exit (e.g. unknown argument)
 # TODO test AWS credentials error (not found, session expired, invalid key id, secret, session token)
 # TODO test successful --help
 # TODO test erroneous --help
 from aws_kinesis_consumer import ErrorHandler
+
+
+def test_user_interrupting_program(capsys: CaptureFixture):
+    # when
+    ErrorHandler.handle(KeyboardInterrupt())
+
+    # then
+    assert extract_output(capsys) == {
+        'stdout': [],
+        'stderr': [],
+    }
 
 
 def test_unexpected_error(capsys: CaptureFixture):
@@ -17,20 +29,26 @@ def test_unexpected_error(capsys: CaptureFixture):
     assert extract_output(capsys) == {
         'stdout': [],
         'stderr': [
-            'ERROR: the program stopped due to the following issue.',
-            'Unexpected error',
+            "ERROR: the program stopped due to the following issue.",
+            "RuntimeError('Unexpected error')",
         ],
     }
 
 
-def test_user_interrupting_program(capsys: CaptureFixture):
+def test_missing_aws_region(capsys: CaptureFixture):
     # when
-    ErrorHandler.handle(KeyboardInterrupt())
+    with raises(SystemExit) as raised_error:
+        ErrorHandler.handle(NoRegionError())
 
     # then
+    assert is_exit_program_error(raised_error)
     assert extract_output(capsys) == {
         'stdout': [],
-        'stderr': [],
+        'stderr': [
+            "ERROR: AWS region has not been found.",
+            "Please pass the region using the environment variable AWS_DEFAULT_REGION. Example:",
+            "$ AWS_DEFAULT_REGION=eu-central-1 aws-kinesis-consumer --stream-name MyStream",
+        ],
     }
 
 
