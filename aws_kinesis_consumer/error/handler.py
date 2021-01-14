2058b3a59b7fd76198b48397886aaf83f9abd4d2
Thierry Nowak <thinow@gmail.com>
2021-01-14 22:06:44 +0100

    error: handle user interruptions

--- aws_kinesis_consumer/error/handler.py
+++ aws_kinesis_consumer/error/handler.py
@@ -6,8 +6,9 @@ class ErrorHandler:
     @classmethod
     def handle(cls, error: BaseException) -> None:
         # User intentionally interrupts the program. Ignore the exception and exit.
-        # if isinstance(error, KeyboardInterrupt):
-        #     pass
-        print(f'ERROR: the program stopped due to the following issue.', file=sys.stderr)
-        print(error, file=sys.stderr)
-        sys.exit(1)
+        if isinstance(error, KeyboardInterrupt):
+            pass
+        else:
+            print(f'ERROR: the program stopped due to the following issue.', file=sys.stderr)
+            print(error, file=sys.stderr)
+            sys.exit(1)
--- tests/error/test_handler.py
+++ tests/error/test_handler.py
@@ -1,30 +1,47 @@
-import pytest
+from pytest import CaptureFixture, raises
 
-from aws_kinesis_consumer import ErrorHandler
-
-handler = ErrorHandler()
-
-
-def test_unexpected_error(capsys):
-    # when
-    with pytest.raises(SystemExit) as raised_error:
-        handler.handle(RuntimeError('Unexpected error'))
-
-    # then
-    output = capsys.readouterr()
-    assert output.out.splitlines() == []
-    assert output.err.splitlines() == [
-        'ERROR: the program stopped due to the following issue.',
-        'Unexpected error',
-    ]
-
-    assert isinstance(raised_error.value, SystemExit)
-    assert raised_error.value.code == 1
-
-# TODO test key interrupt
 # TODO test NoRegionError
 # TODO test AWS credentials error (not found, session expired, invalid key id, secret, session token)
-# TODO test exit code
-# TODO test any unknown exception
 # TODO test successful --help
 # TODO test erroneous --help
+from aws_kinesis_consumer import ErrorHandler
+
+
+def test_unexpected_error(capsys: CaptureFixture):
+    # when
+    with raises(SystemExit) as raised_error:
+        ErrorHandler.handle(RuntimeError('Unexpected error'))
+
+    # then
+    assert is_exit_program_error(raised_error)
+    assert extract_output(capsys) == {
+        'stdout': [],
+        'stderr': [
+            'ERROR: the program stopped due to the following issue.',
+            'Unexpected error',
+        ],
+    }
+
+
+def test_user_interrupting_program(capsys: CaptureFixture):
+    # when
+    ErrorHandler.handle(KeyboardInterrupt())
+
+    # then
+    assert extract_output(capsys) == {
+        'stdout': [],
+        'stderr': [],
+    }
+
+
+def is_exit_program_error(exception_info) -> bool:
+    exception = exception_info.value
+    return isinstance(exception, SystemExit) and exception.code == 1
+
+
+def extract_output(capsys: CaptureFixture):
+    captured = capsys.readouterr()
+    return {
+        'stdout': captured.out.splitlines(),
+        'stderr': captured.err.splitlines(),
+    }
