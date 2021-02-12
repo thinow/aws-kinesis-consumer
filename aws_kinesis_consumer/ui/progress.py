67ea513e9c511c5848fc151edd79dee9674c7164
Thierry Nowak <thinow@gmail.com>
2021-02-12 21:46:26 +0100

    rename internal property

--- aws_kinesis_consumer/ui/progress.py
+++ aws_kinesis_consumer/ui/progress.py
@@ -3,21 +3,21 @@ import sys
 
 class Progress:
     def __init__(self, text: str, max_value: int) -> None:
-        self.progress = 0
+        self.current = 0
         self.goal = max_value
         self.text = text
         self.file = sys.stderr
 
     def increment_and_print(self):
-        self.progress = self.progress + 1
+        self.current = self.current + 1
         self.print()
 
     def print(self) -> None:
-        replace_line = self.progress < self.goal
+        replace_line = self.current < self.goal
         if replace_line:
             print(self.get_printable(), file=self.file, flush=False, end='\r')
         else:
             print(self.get_printable(), file=self.file, flush=True)
 
     def get_printable(self) -> str:
-        return f'<{self.text} {self.progress}/{self.goal}>'
+        return f'<{self.text} {self.current}/{self.goal}>'
