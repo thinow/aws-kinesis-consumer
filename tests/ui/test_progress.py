6741e043172ba5a28aaf36d84dcb3b5c23df4501
Thierry Nowak <thinow@gmail.com>
2021-02-12 21:46:07 +0100

    unit tests progress

---
+++ tests/ui/test_progress.py
@@ -0,0 +1,75 @@
+import sys
+
+from pytest import CaptureFixture
+
+from aws_kinesis_consumer.ui.progress import Progress
+
+
+def test_initial_progress_value(capsys: CaptureFixture):
+    # when
+    Progress('TEXT', max_value=3).print()
+
+    # then
+    force_end_of_capture()
+    assert extract_final_output(capsys) == [
+        '<TEXT 0/3>'
+    ]
+
+
+def test_first_call_to_increment_and_print(capsys: CaptureFixture):
+    # when
+    Progress('TEXT', max_value=3).increment_and_print()
+
+    # then
+    force_end_of_capture()
+    assert extract_final_output(capsys) == [
+        '<TEXT 1/3>'
+    ]
+
+
+def test_consecutive_calls_to_increment_and_print(capsys: CaptureFixture):
+    # when
+    progress = Progress('TEXT', max_value=3)
+    progress.increment_and_print()
+    progress.increment_and_print()
+
+    # then
+    force_end_of_capture()
+    assert extract_final_output(capsys) == [
+        '<TEXT 2/3>',
+    ]
+
+
+def test_extra_calls_to_increment_and_print(capsys: CaptureFixture):
+    # when
+    progress = Progress('TEXT', max_value=3)
+    progress.increment_and_print()
+    progress.increment_and_print()
+    progress.increment_and_print()
+    progress.increment_and_print()  # 4 calls = one more call than max_value
+
+    # then
+    force_end_of_capture()
+    assert extract_final_output(capsys) == [
+        '<TEXT 3/3>',
+        '<TEXT 4/3>',
+    ]
+
+
+def force_end_of_capture():
+    print('', file=sys.stdout)
+    print('', file=sys.stderr)
+
+
+def extract_final_output(capsys: CaptureFixture):
+    captured = capsys.readouterr()
+    return split_lines(captured.err)
+
+
+def split_lines(output: str) -> list:
+    lines = output.splitlines(keepends=True)
+    lines = filter(lambda l: l.endswith('\n'), lines)  # filters out lines about to be replaced
+    lines = map(lambda l: l.replace('\r', ''), lines)  # removes end-of-line character
+    lines = map(lambda l: l.replace('\n', ''), lines)  # removes end-of-line character
+    lines = filter(lambda l: len(l) != 0, lines)  # filters out empty lines
+    return list(lines)
