bac2da6de4fdc474f2c9e246918a97a6a64daef4
Thierry Nowak <thinow@gmail.com>
2021-02-20 20:47:46 +0100

    region: use region configuration when connecting to Kinesis

--- aws_kinesis_consumer/aws/aws_services_factory.py
+++ aws_kinesis_consumer/aws/aws_services_factory.py
@@ -7,4 +7,8 @@ from aws_kinesis_consumer.configuration.configuration import Configuration
 class AWSServicesFactory:
 
     def create_kinesis(self, configuration: Configuration) -> Kinesis:
-        return boto3.client('kinesis', endpoint_url=configuration.endpoint)
+        return boto3.client(
+            'kinesis',
+            endpoint_url=configuration.endpoint,
+            region_name=configuration.region,
+        )
